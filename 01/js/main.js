58291b1d81e407cdbe5fcc811906dfae0b6653db
TaikeiOokawa <ohkawa@barnet.co.jp>
2019-12-13 16:12:37 +0900

    dotinstall #3 Completed

--- 01/js/main.js
+++ 01/js/main.js
@@ -5,7 +5,11 @@
     var vm = new Vue({
         el: '#app',
         data: {
-            name: 'taguchi'
+            todos: [
+                'task 1',
+                'task 2',
+                'task 3'
+            ]
         }
     });
 })();
